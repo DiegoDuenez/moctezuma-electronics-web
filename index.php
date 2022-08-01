1ecd7b2264581ad977abb51d6c75a9e2b9abbcf4
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-08-01 10:42:32 -0500

    add: Se agrego el component de banner

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="libs/glider/glider.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Moctezuma Electronics México</title>
+</head>
+<body>
+
+  <!-- BANNER START -->
+  <?php include_once 'templates/banner.php' ?>
+  <!-- BANNER END -->
+
+
+<h1>
+    Hola
+</h1>
+    
+<script src="libs/glider/glider.min.js"></script>
+<script src="js/banner.js"></script>
+
+</body>
+</html>
---
+++ templates/banner.php
@@ -0,0 +1,19 @@
+<div class="banner__container">
+    <div class="glider">
+        <div class="banner__image">
+            <img src="resources/img/BANNER1.jpg" alt="Sicove">
+        </div>
+        <div class="banner__image">
+            <img src="resources/img/BANNER2.jpg" alt="Sicove">
+        </div>
+        <div class="banner__image">
+            <img src="resources/img/BANNER3.jpg" alt="Sicove">
+        </div>
+        <div class="banner__image">
+            <img src="resources/img/BANNER4.jpg" alt="Sicove">
+        </div>
+        <div class="banner__image">
+            <img src="resources/img/BANNER5.jpg" alt="Sicove">
+        </div>
+    </div>
+</div>
