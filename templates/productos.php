8176ab757a6fc7037cd8b5f856735dbf64f0ae2f
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-08-08 14:00:24 -0500

    add: productos

--- templates/productos.php
+++ templates/productos.php
@@ -7,7 +7,7 @@
 
     <div class="productos__productos-list">
 
-        <div class="productos__producto">
+        <div class="productos__producto ">
             <div class="productos__producto-img">
                 <img src="resources/img/CAMARA1.jpg" alt="">
             </div>
@@ -23,9 +23,66 @@
                         VER MÁS
                     </button>
                 </div>
-
             </div>
         </div>
+
+        <div class="productos__producto productos__producto--reverse">
+            <div class="productos__producto-img">
+                <img src="resources/img/CAMARA2.jpg" alt="">
+            </div>
+            <div class="productos__producto-content">
+                <div class="productos__producto-header">
+                    <p>CÁMARA DE&nbsp;<span>GAMA ELITE</span></p> 
+                </div>
+                <div class="productos__producto-body">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis molestiae libero expedita hic ab
+                    aut ex accusantium similique atque. Rem rerum distinctio sint facere illo tempore nesciunt eveniet praesentium dolore.
+                    
+                    <button class="button">
+                        VER MÁS
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="productos__producto ">
+            <div class="productos__producto-img">
+                <img src="resources/img/CAMARA3.jpg" alt="">
+            </div>
+            <div class="productos__producto-content">
+                <div class="productos__producto-header">
+                    <p>CÁMARA DE&nbsp;<span>GAMA SELECT</span></p> 
+                </div>
+                <div class="productos__producto-body">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis molestiae libero expedita hic ab
+                    aut ex accusantium similique atque. Rem rerum distinctio sint facere illo tempore nesciunt eveniet praesentium dolore.
+                    
+                    <button class="button">
+                        VER MÁS
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="productos__producto productos__producto--reverse">
+            <div class="productos__producto-img">
+                <img src="resources/img/CAMARA4.jpg" alt="">
+            </div>
+            <div class="productos__producto-content">
+                <div class="productos__producto-header">
+                    <p>CÁMARA DE&nbsp;<span>GAMA SELECT</span></p> 
+                </div>
+                <div class="productos__producto-body">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis molestiae libero expedita hic ab
+                    aut ex accusantium similique atque. Rem rerum distinctio sint facere illo tempore nesciunt eveniet praesentium dolore.
+                    
+                    <button class="button">
+                        VER MÁS
+                    </button>
+                </div>
+            </div>
+        </div>
+        
 
     </div>
     
