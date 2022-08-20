c9a3fc03649cc7f81dd68471582e799776b7526e
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-08-20 11:53:04 -0500

    fix: Se quito glider duplicado

--- templates/components/menu.php
+++ templates/components/menu.php
@@ -1,60 +1,44 @@
 <div class="menu menu--justify-end menu--fixed menu--hide" id="menuHide">
-        <a href="#contacto" class="menu__item menu__item--blue">
-            Contacto
-        </a>
-        <a href="#nuestraempresa" class="menu__item" data-scroll>
-            Nuestra empresa
-        </a>
-        <a href="#productos" class="menu__item">
-            Productos
-        </a>
-            <div class="menu__productos-opciones menu__productos-opciones--fixed">
-                <a href="#productos" class="menu__item">
-                    Sistemas de camára/monitor
-                </a>
-                <a href="#productos" class="menu__item">
-                    Detección de obstáculos por radar
-                </a>
-                <a href="#productos" class="menu__item">
-                Sistema de visión 360°
-                </a>
-                <a href="#productos" class="menu__item">
-                    Alarmas de reversa de ruido blanco
-                </a>
-                <a href="#productos" class="menu__item">
-                    Sensores ultrasónicos
-                </a>
-                <a href="#productos" class="menu__item">
-                    Grabador digital móvil
-                </a>
-            </div>
-        <a href="#recomendaciones" class="menu__item">
-            Guías del comprador
-        </a>
-        <a href="#industrias" class="menu__item">
-            Industrias
-        </a>
-        <a href="#servicios" class="menu__item">
-            Servicios
-        </a>
-        <a href="sindocumento.pdf" download class="menu__item">
-            Catálogo de productos
-        </a>
-    </div>
-    <div class="glider">
-        <div class="card">
-            <img src="resources/img/BANNER1.jpg" alt="">
+    <a href="#contacto" class="menu__item menu__item--blue">
+        Contacto
+    </a>
+    <a href="#nuestraempresa" class="menu__item" data-scroll>
+        Nuestra empresa
+    </a>
+    <a href="#productos" class="menu__item">
+        Productos
+    </a>
+        <div class="menu__productos-opciones menu__productos-opciones--fixed">
+            <a href="#productos" class="menu__item">
+                Sistemas de camára/monitor
+            </a>
+            <a href="#productos" class="menu__item">
+                Detección de obstáculos por radar
+            </a>
+            <a href="#productos" class="menu__item">
+            Sistema de visión 360°
+            </a>
+            <a href="#productos" class="menu__item">
+                Alarmas de reversa de ruido blanco
+            </a>
+            <a href="#productos" class="menu__item">
+                Sensores ultrasónicos
+            </a>
+            <a href="#productos" class="menu__item">
+                Grabador digital móvil
+            </a>
         </div>
-        <div class="card">
-            <img src="resources/img/BANNER2.jpg" alt="">
-        </div>
-        <div class="card">
-            <img src="resources/img/BANNER3.jpg" alt="">
-        </div>
-        <div class="card">
-            <img src="resources/img/BANNER4.jpg" alt="">
-        </div>
-        <div class="card">
-            <img src="resources/img/BANNER5.jpg" alt="">
-        </div>
-    </div>
+    <a href="#recomendaciones" class="menu__item">
+        Guías del comprador
+    </a>
+    <a href="#industrias" class="menu__item">
+        Industrias
+    </a>
+    <a href="#servicios" class="menu__item">
+        Servicios
+    </a>
+    <a href="sindocumento.pdf" download class="menu__item">
+        Catálogo de productos
+    </a>
+</div>
+   
