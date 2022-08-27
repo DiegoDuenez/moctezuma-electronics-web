0da915d65169abca243233e85a0c5e555f6b9572
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-08-27 12:07:34 -0500

    remove:

--- templates/layouts/servicios.php
+++ templates/layouts/servicios.php
@@ -4,43 +4,42 @@
         <h2 class="title">SERVICIOS</h2>
     </div>
 
-        <div class="servicios__card-container">
-            <div class="servicios__card-row">
-                <div class="servicios__card">
-                    <div class="servicios__card-body">
-                        <img src="resources/img/SERVICIOS1.jpg" alt="">
-                    </div>
-                    <div class="servicios__card-footer">
-                        CONSULTORÍA
-                    </div>
+    <div class="servicios__card-container">
+        <div class="servicios__card-row">
+            <div class="servicios__card">
+                <div class="servicios__card-body">
+                    <img src="resources/img/SERVICIOS1.jpg" alt="Consultoría" title="Consultoría">
                 </div>
-                <div class="servicios__card">
-                    <div class="servicios__card-body">
-                        <img src="resources/img/SERVICIOS2.jpg" alt="">
-                    </div>
-                    <div class="servicios__card-footer servicios__card-footer--blue">
-                        CAPACITACIÓN
-                    </div>
+                <div class="servicios__card-footer">
+                    CONSULTORÍA
                 </div>
-                <div class="servicios__card">
-                    <div class="servicios__card-body">
-                        <img src="resources/img/SERVICIOS3.jpg" alt="">
-                    </div>
-                    <div class="servicios__card-footer">
-                        INSTALACIÓN
-                    </div>
+            </div>
+            <div class="servicios__card">
+                <div class="servicios__card-body">
+                    <img src="resources/img/SERVICIOS2.jpg" alt="Capacitación" title="Capacitación">
                 </div>
-                <div class="servicios__card">
-                    <div class="servicios__card-body">
-                        <img src="resources/img/SERVICIOS4.jpg" alt="">
-                    </div>
-                    <div class="servicios__card-footer servicios__card-footer--blue">
-                        GARANTÍA
-                    </div>
+                <div class="servicios__card-footer servicios__card-footer--blue">
+                    CAPACITACIÓN
+                </div>
+            </div>
+            <div class="servicios__card">
+                <div class="servicios__card-body">
+                    <img src="resources/img/SERVICIOS3.jpg" alt="Instalación" title="Instalación">
+                </div>
+                <div class="servicios__card-footer">
+                    INSTALACIÓN
+                </div>
+            </div>
+            <div class="servicios__card">
+                <div class="servicios__card-body">
+                    <img src="resources/img/SERVICIOS4.jpg" alt="Garantía" title="Garantía" >
+                </div>
+                <div class="servicios__card-footer servicios__card-footer--blue">
+                    GARANTÍA
                 </div>
             </div>
         </div>
     </div>
-
-
+    
 </div>
+
