0b16916d527c9e7111dca64973540c19adab0aba
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-08-30 15:34:08 -0500

    add: Se agregaron los links a los dos menus

--- templates/components/menu.php
+++ templates/components/menu.php
@@ -8,22 +8,22 @@
             Productos
         </button>
             <div class="menu__productos-opciones">
-                <button class="menu__item" data-href="sistemas_camara_monitor">
+                <button class="menu__item" data-href="productos" data-producto='sistemas_camara_monitor' onclick="openTab(event, 'sistemas_camara_monitor')">
                     Sistemas de camára/monitor
                 </button>
-                <button class="menu__item" data-href="deteccion_obstaculos_radar">
+                <button class="menu__item" data-href="productos" data-producto='deteccion_obstaculos_radar' onclick="openTab(event, 'deteccion_obstaculos_radar')">
                     Detección de obstáculos por radar
                 </button>
-                <button class="menu__item" data-href="sistema_vision_360">
+                <button class="menu__item" data-href="productos" data-producto='sistema_vision_360' onclick="openTab(event, 'sistema_vision_360')">
                     Sistema de visión 360°
                 </button>
-                <button class="menu__item" data-href="alarmas_reversa_ruido_blanco">
+                <button class="menu__item" data-href="productos" data-producto='alarmas_reversa_ruido_blanco' onclick="openTab(event, 'alarmas_reversa_ruido_blanco')">
                     Alarmas de reversa de ruido blanco
                 </button>
-                <button class="menu__item" data-href="sensores_ultrasonicos">
+                <button class="menu__item" data-href="productos" data-producto='sensores_ultrasonicos' onclick="openTab(event, 'sensores_ultrasonicos')">
                     Sensores ultrasónicos
                 </button>
-                <button class="menu__item" data-href="#grabador_digital_movil">
+                <button class="menu__item" data-href="productos" data-producto='grabador_digital_movil' onclick="openTab(event, 'grabador_digital_movil')">
                     Grabador digital móvil
                 </button>
             </div>
