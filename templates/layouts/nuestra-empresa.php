f488ca9a2ea5b905d509253c4ebf01f47d7cb186
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-10-13 11:22:13 -0500

    add: Add mision and vision in nuestra empresa and add mask when hover on servicios card

--- templates/layouts/nuestra-empresa.php
+++ templates/layouts/nuestra-empresa.php
@@ -29,23 +29,92 @@
 
         <div class="nuestra-empresa__content-text">
             <p>
-                <span style="color: var(--color-blue)">Moctezuma Electronics México</span> tiene el firme propósito de que las carreteras y zonas de trabajo sean seguras para todos.
+                <span style="color: var(--color-blue)">Moctezuma Electronics México</span> como único distribuidor y representante 
+                comercial de primer nivel de la marca BRIGADE en México y Centroamérica 
+                tiene el firme propósito de que las carreteras y zonas de trabajo sean
+                seguras para todos.
             </p>
             <p>
-                Esté donde esté en el mundo, los vehiculos de gran tamaño para carreteras y las máquinas de construcción siguen siendo un peligro. Por suerte,
-                la cultura de la salud y seguridad está poco a poco mejorando para luchar contra estos peligros, pero independientemente de la obligación local, 
-                no hay excusas para poner en peligro la seguridad.
-            </p>
-            <p>
-                En <span style="color: var(--color-blue)">Moctezuma Electronics México</span> sabemos que sin productos fiables de alta calidad, no seríamos la empresa que somos hoy en día.
-            </p>
-            <p>
-                Este es el motivo por el que hacemos ese esfuerzo extraordinario para que no se comprometa la seguridad.
+                Esté donde esté en el mundo, los vehículos de 
+                gran tamaño para carreteras y las máquinas de 
+                construcción y minería siguen siendo un peligro. 
+                Por suerte, la cultura de la salud y seguridad 
+                está poco a poco mejorando para luchar contra estos 
+                peligros, pero independientemente de la normatividad de 
+                seguridad de cada país, no hay excusas para poner en peligro 
+                la seguridad del personal que opera día tras día estas enormes maquinas.
             </p>
         </div>
         <div class="nuestra-empresa__content-img">
         </div>
+    </div>
+
+    <div class="nosotros">
+
+        <div class="nosotros__content">
+
+            <div class="nosotros__content-header">
+
+                <div class="nuestra-empresa__card-icon">
+                    <i class="fa-regular fa-bullseye-arrow"></i>
+                </div>
+                <div class="nuestra-empresa__card-text">
+                    Misión
+                </div>
+
+            </div>
+            <div class="nosotros__content-body">
+                Promover y participar en el impulso y utilización de lo último en tecnología 
+                para lograr maniobras seguras en vehículos pesados e Integración con 
+                soluciones existentes para controlar y minimizar riesgos en la operación 
+                diaria dentro de la empresa.
+            </div>
+
+        </div>
+
+        <div class="nosotros__content">
+
+            <div class="nosotros__content-header">
+
+                <div class="nuestra-empresa__card-icon">
+                    <i class="fa-solid fa-eye"></i>
+                </div>
+                <div class="nuestra-empresa__card-text">
+                    Visión
+                </div>
+
+            </div>
+            <div class="nosotros__content-body">
+                Ser líderes en soluciones, servicios de sistemas y 
+                dispositivos que faciliten la maniobra de diversos tipos 
+                de vehículos utilizados en la industria basados en la 
+                eliminación de puntos ciegos al operador, así como, 
+                de alertamiento de obstáculos al conductor durante la operación diaria.
+            </div>
+
+        </div>
 
     </div>
+
+    <!-- <div class="nuestra-empresa__icons">
+        <div class="nuestra-empresa__card">
+            <div class="nuestra-empresa__card-icon">
+                <i class="fa-regular fa-bullseye-arrow"></i>
+            </div>
+            <div class="nuestra-empresa__card-text">
+                Misión
+            </div>
+        </div>
+
+        <div class="nuestra-empresa__card">
+            <div class="nuestra-empresa__card-icon">
+                <i class="fa-solid fa-eye"></i>
+            </div>
+            <div class="nuestra-empresa__card-text">
+                Visión
+            </div>
+        </div>
+    </div> -->
+
 
 </div>
--- templates/layouts/servicios.php
+++ templates/layouts/servicios.php
@@ -13,6 +13,16 @@
                 <div class="servicios__card-footer">
                     CONSULTORÍA
                 </div>
+                <div class="servicios__card-mask">
+                    <div class="servicios__card-mask-body">
+                        Nuestro equipo de especialistas posee los criterios precisos 
+                        para comprender la situación de la empresa y sugerir una propuesta rentable, 
+                        el alto nivel de profesionalidad de nuestros expertos se basa en una sólida 
+                        y amplia experiencia práctica y en la formación continua en las mejores 
+                        y más modernas prácticas internacionales.
+                    </div>
+                </div>
+
             </div>
             <div class="servicios__card">
                 <div class="servicios__card-body">
@@ -20,6 +30,14 @@
                 </div>
                 <div class="servicios__card-footer servicios__card-footer--blue">
                     CAPACITACIÓN
+                </div>
+                <div class="servicios__card-mask">
+                    <div class="servicios__card-mask-body">
+                        Contamos con excelente personal capacitado para mostrar a directivos 
+                        y conductores de una manera sencilla y practica las funcionalidades 
+                        de nuestro abanico de soluciones de seguridad antes y después de realizada la implementación 
+                        en las unidades de la empresa.
+                    </div>
                 </div>
             </div>
             <div class="servicios__card">
@@ -29,6 +47,16 @@
                 <div class="servicios__card-footer">
                     INSTALACIÓN
                 </div>
+                <div class="servicios__card-mask">
+                    <div class="servicios__card-mask-body">
+                        El equipo técnico con que contamos está capacitado para realizar 
+                        la instalación de nuestros sistemas de seguridad para maniobras en 
+                        sus vehículos de una manera adecuada, de tal manera que cumpla con 
+                        los estándares de calidad que exige la marca en caso de necesitarse 
+                        una posible validación de garantías. Esto puede darle un beneficio 
+                        adicional a la inversión realizada en la implementación de nuestros sistemas.
+                    </div>
+                </div>
             </div>
             <div class="servicios__card">
                 <div class="servicios__card-body">
@@ -36,6 +64,18 @@
                 </div>
                 <div class="servicios__card-footer servicios__card-footer--blue">
                     GARANTÍA
+                </div>
+                <div class="servicios__card-mask">
+                    <div class="servicios__card-mask-body">
+                        En Moctezuma Electronics México sabemos que, 
+                        sin productos fiables de alta calidad, no seríamos la empresa que 
+                        somos hoy en día. Este es el motivo por el que hacemos ese esfuerzo 
+                        extraordinario para que no se comprometa la seguridad del personal 
+                        en sus centros de trabajo. Por este motivo nuestras garantías según 
+                        el producto del que se trate puede ser desde 1, 2, 3, 5 años incluso 
+                        hasta vitalicia, siempre y cuando cumpla con los requisitos de 
+                        una instalación y uso apropiado del producto.
+                    </div>
                 </div>
             </div>
         </div>
